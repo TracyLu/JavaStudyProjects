f710feda277338d52130371e09b46da232393d45
Tracy <lu-hailing@hotmail.com>
2012-09-18 18:25:04 +0800

    Refined task meta head information length

--- src/main/java/net/madz/download/service/metadata/Consts.java
+++ src/main/java/net/madz/download/service/metadata/Consts.java
@@ -2,11 +2,6 @@ package net.madz.download.service.metadata;
 
 public class Consts {
 
-	/***
-	 * For below resources length, they are measured in bytes.
-	 */
-	// For URL, ReferURL, Folder: 1000 characters, 1 character is 3 bytes.
-	//
 	public static final int URL_LENGTH = 1000 * 3;
 	public static final int REFER_URL_LENGTH = 1000 * 3;
 	public static final int FOLDER_LENGTH = 1000 * 3;
@@ -16,7 +11,20 @@ public class Consts {
 	public static final int RESUMABLE_FLAG_LENGTH = 1;
 	public static final int THREAD_NUMBER_LENGTH = 1;
 	public static final int STATE_LENGTH = 1;
-	
-	
+	public static int URL_POSITION = 0;
+	public static int REFER_URL_POSITION = URL_POSITION + URL_LENGTH;
+	public static int FOLDER_POSITION = REFER_URL_POSITION + REFER_URL_LENGTH;
+	public static int FILE_NAME_POSITION = FOLDER_POSITION + FOLDER_LENGTH;
+	public static int TOTAL_LENGTH_POSITION = FILE_NAME_POSITION
+			+ FILE_NAME_LENGTH;
+	public static int SEGMENTS_NUMBER_POSITION = TOTAL_LENGTH_POSITION
+			+ TOTAL_LENGTH;
+	public static int RESUMABLE_FLAG_POSITION = SEGMENTS_NUMBER_POSITION
+			+ SEGMENTS_NUMBER_LENGTH;
+	public static int THREAD_NUMBER_POSITION = RESUMABLE_FLAG_POSITION
+			+ RESUMABLE_FLAG_LENGTH;
+	public static int STATE_POSTION = THREAD_NUMBER_POSITION
+			+ THREAD_NUMBER_LENGTH;
+	public static int FIRST_SEGMENT_POSITION = STATE_POSTION + STATE_LENGTH;
 
 }
--- src/main/java/net/madz/download/service/metadata/MetaManager.java
+++ src/main/java/net/madz/download/service/metadata/MetaManager.java
@@ -11,42 +11,32 @@ public class MetaManager {
 		RandomAccessFile randomAccessFile = null;
 		try {
 			randomAccessFile = new RandomAccessFile(file, "rw");
-			int position = 0;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.URL_POSITION);
 			randomAccessFile.writeChars(task.getUrl().toString());
 
-			position = position + Consts.URL_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.REFER_URL_LENGTH);
 			randomAccessFile.writeChars(task.getReferURL().toString());
 
-			position = position + Consts.REFER_URL_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.FOLDER_POSITION);
 			randomAccessFile.writeChars(task.getFolder().getAbsolutePath());
 
-			position = position + Consts.FOLDER_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.FILE_NAME_POSITION);
 			randomAccessFile.writeChars(task.getFileName());
 
-			position = position + Consts.FILE_NAME_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.TOTAL_LENGTH_POSITION);
 			randomAccessFile.writeLong(task.getTotalLength());
 
-			position = position + Consts.TOTAL_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.SEGMENTS_NUMBER_POSITION);
 			randomAccessFile.writeInt(task.getSegmentsNumber());
 
-			position = position + Consts.SEGMENTS_NUMBER_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.RESUMABLE_FLAG_POSITION);
 			randomAccessFile.writeBoolean(task.isResumable());
 
-			position = position + Consts.RESUMABLE_FLAG_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.THREAD_NUMBER_POSITION);
 			randomAccessFile.writeByte(task.getThreadNumber());
 
-			position = position + Consts.THREAD_NUMBER_LENGTH;
-			randomAccessFile.seek(position);
+			randomAccessFile.seek(Consts.STATE_POSTION);
 			randomAccessFile.writeByte(task.getState());
-			
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -64,72 +54,55 @@ public class MetaManager {
 	public static void deserialize(File file) {
 		RandomAccessFile randomAccessFile = null;
 		StringBuilder headInfo = new StringBuilder();
-		int pointer = 0;
 		try {
 			randomAccessFile = new RandomAccessFile(file, "rw");
+			
 			byte[] result = new byte[Consts.URL_LENGTH];
 			randomAccessFile.readFully(result, 0, Consts.URL_LENGTH);
 			headInfo.append(" URL:");
 			headInfo.append(new String(result));
-			pointer = pointer + Consts.URL_LENGTH;
-			
+
 			result = new byte[Consts.REFER_URL_LENGTH];
-			randomAccessFile.seek(pointer);
+			randomAccessFile.seek(Consts.REFER_URL_POSITION);
 			randomAccessFile.readFully(result, 0, Consts.REFER_URL_LENGTH);
 			headInfo.append(" Refer URL:");
 			headInfo.append(new String(result));
-			pointer += Consts.REFER_URL_LENGTH;
-			
+
 			result = new byte[Consts.FOLDER_LENGTH];
-			randomAccessFile.seek(pointer); 
+			randomAccessFile.seek(Consts.FOLDER_POSITION);
 			randomAccessFile.readFully(result, 0, Consts.FOLDER_LENGTH);
 			headInfo.append(" Folder:");
 			headInfo.append(new String(result));
-			pointer += Consts.FOLDER_LENGTH;
-			
+
 			result = new byte[Consts.FILE_NAME_LENGTH];
-			randomAccessFile.seek(pointer); 
+			randomAccessFile.seek(Consts.FILE_NAME_POSITION);
 			randomAccessFile.readFully(result, 0, Consts.FILE_NAME_LENGTH);
 			headInfo.append(" File name:");
 			headInfo.append(new String(result));
-			pointer += Consts.FILE_NAME_LENGTH;
-			
-			randomAccessFile.seek(pointer); 
+
+			randomAccessFile.seek(Consts.TOTAL_LENGTH_POSITION);
 			headInfo.append(" Total Length:");
 			headInfo.append(randomAccessFile.readLong());
-			pointer += Consts.TOTAL_LENGTH;
-			
-			result = new byte[Consts.SEGMENTS_NUMBER_LENGTH];
-			randomAccessFile.seek(pointer); 
-			randomAccessFile.readFully(result, 0, Consts.SEGMENTS_NUMBER_LENGTH);
+
+			randomAccessFile.seek(Consts.SEGMENTS_NUMBER_POSITION);
 			headInfo.append(" Segements Number:");
-			headInfo.append(new String(result));
-			pointer += Consts.SEGMENTS_NUMBER_LENGTH;
-			
-			result = new byte[Consts.RESUMABLE_FLAG_LENGTH];
-			randomAccessFile.seek(pointer); 
-			randomAccessFile.readFully(result, 0, Consts.RESUMABLE_FLAG_LENGTH);
+			headInfo.append(randomAccessFile.readInt());
+
+			randomAccessFile.seek(Consts.RESUMABLE_FLAG_POSITION);
 			headInfo.append(" Resumable:");
-			headInfo.append(new String(result));
-			pointer += Consts.RESUMABLE_FLAG_LENGTH;
+			headInfo.append(randomAccessFile.readByte());
 			
-			result = new byte[Consts.THREAD_NUMBER_LENGTH];
-			randomAccessFile.seek(pointer); 
-			randomAccessFile.readFully(result, 0, Consts.THREAD_NUMBER_LENGTH);
+			randomAccessFile.seek(Consts.THREAD_NUMBER_POSITION);
 			headInfo.append(" Thread Number:");
-			headInfo.append(new String(result));
-			pointer += Consts.THREAD_NUMBER_LENGTH;
+			headInfo.append(randomAccessFile.readByte());
 			
-			result = new byte[Consts.STATE_LENGTH];
-			randomAccessFile.seek(pointer); 
-			randomAccessFile.readFully(result, 0, Consts.STATE_LENGTH);
+			randomAccessFile.seek(Consts.STATE_POSTION);
 			headInfo.append(" State:");
-			headInfo.append(new String(result));
-			pointer += Consts.STATE_LENGTH;
+			headInfo.append(randomAccessFile.readByte());
 			
 			System.out.println("Task header Information:");
 			System.out.println(headInfo.toString());
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -137,8 +110,7 @@ public class MetaManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
