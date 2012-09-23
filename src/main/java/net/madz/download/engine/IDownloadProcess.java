e92d1b405bde7ac409eaebfad3a5e24accfb4d29
Tracy <lu-hailing@hotmail.com>
2012-09-23 23:50:49 +0800

    Add preconditions and postconditions.

--- src/main/java/net/madz/download/engine/IDownloadProcess.java
+++ src/main/java/net/madz/download/engine/IDownloadProcess.java
@@ -57,10 +57,67 @@ public interface IDownloadProcess extends Serializable, IReactiveObject {
     }
 
     public static enum StateEnum implements IState<IDownloadProcess, StateEnum> {
+        /**
+         * Preconditions: 
+         * 
+         * 1. Request Validation Passed
+         * 
+         * 1.1 URL Format legal
+         *  
+         * 1.2 File Path legal
+         *   
+         * 1.2.1 File does not exist
+         * 
+         * 1.2.2 File can be created (write) under it's directory
+         * 
+         * 1.2.2.1 File's directory (exists and can be written OR does not exist but can be created under it's parent directory)
+         * 
+         * Postconditions:
+         * 
+         * 1. Download Task Meta-data file is created.
+         * 
+         * 2. URL, Folder, filename, thread number, state = "New" are stored in the Meta-data file.
+         */
         @Initial
         New,
+        
+        /**
+         * Preconditions:
+         *
+         * 1. Download Task Meta-data file exists.
+         * 
+         * 2. Meta-information in the meta-data file are properly set.
+         * 
+         * 2.1. URL format is legal.
+         * 
+         * 2.2.Folder and filename is legal, as defined in New.Postconditions.
+         * 
+         * 2.3.Thread Number is greater than 0 and less than 20
+         * 
+         * 2.4.State is set legally (New, Inactive)
+         *  
+         * Postconditions:
+         * 
+         * 1. Following information should be reset and set within the meta-data file. 
+         * 
+         * 1.1. Total length is set
+         * 
+         * 1.2. resumable is set
+         * 
+         * 1.3. segments number is set
+         * 
+         * 1.4. state is set to "Prepared".
+         * 
+         * 2. Download Task data file is created 
+         *    or re-created with deleting the pre-existing file (application aborted before update status to Prepared).
+         */ 
         @Running(priority = 1)
-        Queued,
+        Prepared,
+        /**
+         * Preconditions:
+         * 
+         * Postconditions:
+         */
         @Running(priority = 0)
         Started,
         @Corrupted(recoverPriority = 0)
@@ -77,20 +134,20 @@ public interface IDownloadProcess extends Serializable, IReactiveObject {
         Removed;
 
         static {
-            New.transitionFunction.put(Prepare, Queued);
+            New.transitionFunction.put(Prepare, Prepared);
             New.transitionFunction.put(Remove, Removed);
-            Queued.transitionFunction.put(Inactivate, InactiveQueued);
-            Queued.transitionFunction.put(Start, Started);
-            Queued.transitionFunction.put(Pause, Paused);
-            Queued.transitionFunction.put(Remove, Removed);
-            InactiveQueued.transitionFunction.put(Activate, Queued);
+            Prepared.transitionFunction.put(Inactivate, InactiveQueued);
+            Prepared.transitionFunction.put(Start, Started);
+            Prepared.transitionFunction.put(Pause, Paused);
+            Prepared.transitionFunction.put(Remove, Removed);
+            InactiveQueued.transitionFunction.put(Activate, Prepared);
             Started.transitionFunction.put(Receive, Started);
             Started.transitionFunction.put(Pause, Paused);
             Started.transitionFunction.put(Inactivate, InactiveStarted);
             Started.transitionFunction.put(Err, Failed);
             Started.transitionFunction.put(Finish, Finished);
             Started.transitionFunction.put(Remove, Removed);
-            InactiveStarted.transitionFunction.put(Activate, Queued);
+            InactiveStarted.transitionFunction.put(Activate, Prepared);
             Paused.transitionFunction.put(Restart, New);
             Paused.transitionFunction.put(Resume, New);
             Paused.transitionFunction.put(Remove, Removed);
