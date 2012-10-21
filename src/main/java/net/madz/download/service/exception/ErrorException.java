02c63ecc3260fb3f3e559e4e0ac8365df44585aa
Barry Zhong <zhongdj@gmail.com>
2012-10-21 12:07:21 +0800

    Add serialVersionUID to ErrorException class

--- src/main/java/net/madz/download/service/exception/ErrorException.java
+++ src/main/java/net/madz/download/service/exception/ErrorException.java
@@ -2,6 +2,8 @@ package net.madz.download.service.exception;
 
 public class ErrorException extends Exception {
 
+    private static final long serialVersionUID = -3784777249170182306L;
+    
     private String message;
 
     public String getMessage() {
